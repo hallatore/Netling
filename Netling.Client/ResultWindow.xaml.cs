9e12e5d0bcff0bcff3765e516207d7daf56479c0
Tore Lervik <tore@mindre.net>
2013-06-06 21:38:05 +0200

    Added request per second graph to the result window

--- Netling.Client/ResultWindow.xaml.cs
+++ Netling.Client/ResultWindow.xaml.cs
@@ -4,6 +4,9 @@ using System.Globalization;
 using System.Linq;
 using System.Windows;
 using Netling.Core.Models;
+using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Series;
 
 namespace Netling.Client
 {
@@ -26,6 +29,51 @@ namespace Netling.Client
             Title = string.Format("Threads: {0}, runs: {1}, url's: {2}", result.Threads, result.Runs, result.Results.Select(r => r.Url).Distinct().Count());
 
             LoadUrlSummary();
+            LoadGraph();
+        }
+
+        private void LoadGraph()
+        {
+            var result = Result.Results
+                .Where(r => !r.IsError)
+                .GroupBy(r => (r.StartTime.Ticks/10000 + r.ResponseTime) / 1000)
+                .OrderBy(r => r.Key)
+                .ToList();
+
+            var plotModel = new PlotModel
+                {
+                    PlotMargins = new OxyThickness(0)
+                };
+
+            plotModel.Axes.Add(new LinearAxis
+                {
+                    MinimumPadding = 0.0,
+                    MaximumPadding = 0.0,
+                    IsAxisVisible = false,
+                    IsZoomEnabled = false,
+                    IsPanEnabled = false,
+                    Position = AxisPosition.Bottom
+                });
+
+            plotModel.Axes.Add(new LinearAxis
+                {
+                    Minimum = 0.0,
+                    MaximumPadding = 0.1,
+                    MajorGridlineStyle = LineStyle.Solid,
+                    MinorGridlineStyle = LineStyle.Dot,
+                    IsZoomEnabled = false,
+                    IsPanEnabled = false
+                });
+
+            var ls = new LineSeries(OxyColor.Parse("#ff0079c5"));
+
+            foreach (var item in result)
+            {
+                ls.Points.Add(new DataPoint(item.Key, item.Count()));
+            }
+
+            plotModel.Series.Add(ls);
+            RequestsPerSecondGraph.Model = plotModel;
         }
 
         private void LoadUrlSummary()
