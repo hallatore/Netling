61d5a16348095234fc1f6b12bd6bfa9ecb28c5b3
Tore Lervik <tore@lervik.com>
2016-07-09 16:46:40 +0200

    Added custom threading with core afinity for added throughput

--- Netling.Core/PerformanceJob.cs
+++ Netling.Core/PerformanceJob.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Threading;
 using Netling.Core.Models;
 using Netling.Core.Performance;
+using System.Runtime.InteropServices;
 
 namespace Netling.Core
 {
@@ -23,50 +24,51 @@ namespace Netling.Core
             for (var i = 0; i < threads; i++)
             {
                 var resetEvent = new ManualResetEventSlim(false);
-                ThreadPool.QueueUserWorkItem((state) =>
+                QueueThreadWithAfinity(i, () =>
+                {
+                    var result = new JobResult();
+                    var sw2 = new Stopwatch();
+                    var worker = new HttpWorker(url);
+
+                    while (!cancellationToken.IsCancellationRequested && duration.TotalMilliseconds > sw.ElapsedMilliseconds)
                     {
-                        var result = new JobResult();
-                        var sw2 = new Stopwatch();
-                        var worker = new HttpWorker(url);
+                        sw2.Restart();
 
-                        while (!cancellationToken.IsCancellationRequested && duration.TotalMilliseconds > sw.ElapsedMilliseconds)
+                        try
                         {
-                            sw2.Restart();
-
-                            try
+                            if (pipelining == 1)
                             {
-                                if (pipelining == 1)
+                                worker.Write();
+                                worker.Flush();
+                                var length = worker.Read();
+                                result.Add((int)Math.Floor(sw.Elapsed.TotalSeconds), length, (double)sw2.ElapsedTicks / Stopwatch.Frequency * 1000);
+                            }
+                            else
+                            {
+                                for (var j = 0; j < pipelining; j++)
                                 {
                                     worker.Write();
-                                    worker.Flush();
-                                    var length = worker.Read();
+                                }
+
+                                worker.Flush();
+
+                                for (var j = 0; j < pipelining; j++)
+                                {
+                                    var length = worker.ReadPipelined();
                                     result.Add((int)Math.Floor(sw.Elapsed.TotalSeconds), length, (double)sw2.ElapsedTicks / Stopwatch.Frequency * 1000);
                                 }
-                                else { 
-                                    for (var j = 0; j < pipelining; j++)
-                                    {
-                                        worker.Write();
-                                    }
-
-                                    worker.Flush();
-
-                                    for (var j = 0; j < pipelining; j++)
-                                    {
-                                        var length = worker.ReadPipelined();
-                                        result.Add((int)Math.Floor(sw.Elapsed.TotalSeconds), length, (double)sw2.ElapsedTicks / Stopwatch.Frequency * 1000);
-                                    }
-                                }
-                            }
-                            catch (Exception ex)
-                            {
-                                result.AddError((int)Math.Floor(sw.Elapsed.TotalSeconds));
                             }
                         }
+                        catch (Exception ex)
+                        {
+                            result.AddError((int)Math.Floor(sw.Elapsed.TotalSeconds));
+                        }
+                    }
 
-                        results.Enqueue(result);
-                        resetEvent.Set();
-                        totalRuntime = sw.Elapsed.TotalMilliseconds;
-                    }, i);
+                    results.Enqueue(result);
+                    resetEvent.Set();
+                    totalRuntime = sw.Elapsed.TotalMilliseconds;
+                });
 
                 events.Add(resetEvent);
             }
@@ -78,6 +80,40 @@ namespace Netling.Core
             }
             
             return JobResult.Merge(totalRuntime, results.ToList());
+        }
+
+        private void QueueThread(int i, Action action)
+        {
+            ThreadPool.QueueUserWorkItem((s) => {
+                action.Invoke();
+            });
+        }
+
+        private void QueueThreadWithAfinity(int i, Action action)
+        {
+            var thread = new Thread(() => {
+                Thread.BeginThreadAffinity();
+                var afinity = i % Environment.ProcessorCount + 1;
+                CurrentThread.ProcessorAffinity = new IntPtr(afinity);
+                action.Invoke();
+                Thread.EndThreadAffinity();
+            });
+            thread.Start();
+        }
+
+        [DllImport("kernel32.dll")]
+        public static extern int GetCurrentThreadId();
+
+        private ProcessThread CurrentThread
+        {
+            get
+            {
+                int id = GetCurrentThreadId();
+                return
+                    (from ProcessThread th in System.Diagnostics.Process.GetCurrentProcess().Threads
+                     where th.Id == id
+                     select th).Single();
+            }
         }
     }
 }
