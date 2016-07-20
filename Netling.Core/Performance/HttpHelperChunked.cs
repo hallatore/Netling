a58963c4136db26847bd7aa779079e5df715471f
Tore Lervik <tore@lervik.com>
2016-07-20 19:29:33 +0200

    Added helper methods to speed up finding end of chunked stream

--- Netling.Core/Performance/HttpHelperChunked.cs
+++ Netling.Core/Performance/HttpHelperChunked.cs
@@ -1,4 +1,7 @@
-﻿namespace Netling.Core.Performance
+﻿using System.Numerics;
+using Netling.Core.Utils;
+
+namespace Netling.Core.Performance
 {
     internal static class HttpHelperChunked
     {
@@ -14,18 +17,33 @@
                    buffer[end - 1] == 10;
         }
 
+        private static Vector<short> _zeroReturnVector = new Vector<short>(48 + (13 << 8));
+        private static Vector<short> _zeroReturnVectorFiller = new Vector<short>(48 + (13 << 8));
+
         public static int SeekEndOfChunkedStream(byte[] buffer, int start, int end)
         {
-            while (start + 4 < buffer.Length && start + 4 < end)
+            while (start + 4 < end)
             {
-                if (buffer[start + 0] == 48 &&
-                    buffer[start + 1] == 13 &&
-                    buffer[start + 2] == 10 &&
-                    buffer[start + 3] == 13 &&
-                    buffer[start + 4] == 10)
-                    return start + 5;
+                if (!ByteHelpers.ContainsPossibly(buffer, start, end, ref _zeroReturnVector))
+                {
+                    start += Vector<byte>.Count;
+                    continue;
+                }
 
-                start++;
+                var c = 0;
+
+                while (start + 4 < end && c < Vector<byte>.Count)
+                {
+                    if (buffer[start + 0] == 48 &&
+                        buffer[start + 1] == 13 &&
+                        buffer[start + 2] == 10 &&
+                        buffer[start + 3] == 13 &&
+                        buffer[start + 4] == 10)
+                        return start + 5;
+
+                    c++;
+                    start++;
+                }
             }
 
             return -1;
